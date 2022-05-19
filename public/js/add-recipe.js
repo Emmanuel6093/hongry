a3fa0d2267e7899cee02c24e225cd08515cd4c53
Primitive J <hokecj@gmail.com>
2022-05-19 00:23:18 -0400

    created add-recipe.js, delete-recipe.js, main.js, main.handlebars to handle login functionality and data for creating a recipe. NEED INGREDIENTS TO BE ADDED

---
+++ public/js/add-recipe.js
@@ -0,0 +1,35 @@
+//NEEDS CODE-REVISION-
+// ----- ----- ----- ----- EXECUTED IN DASHBOARD TEMPLATE --------------------------------
+
+const addRecipeForm = document.getElementById('addRecipe-form');
+
+async function newRecipeHandler(event) {
+  event.preventDefault();
+
+  // Gets values from form fields
+  const recipe_name = document.getElementById('add-recipe-name').value;
+  const recipe_description = document.getElementById('add-post-desc').value;
+
+  // Fetches using values then inserts data into body (to be extracted in the route (EXAMPLE: req.body.recipe_title) )
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      recipe_name,
+      recipe_description,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  // IF response = OK refresh page. 
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    // Log/Alert error
+    alert(response.statusText);
+  }
+}
+
+// Add the event handler for the form submission
+addRecipeForm.addEventListener('submit', newRecipeHandler);
