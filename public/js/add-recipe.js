eb354c44932f6e7963daff7f3fe1f6893d508eec
Primitive J <hokecj@gmail.com>
2022-05-23 14:35:47 -0400

    added post-recipe.handlebars and started to recode logic in postrecipe.js

--- public/js/add-recipe.js
+++ public/js/add-recipe.js
@@ -3,6 +3,7 @@
 // ----- ----- ----- ----- EXECUTED IN DASHBOARD TEMPLATE --------------------------------//
 
 const addRecipeForm = document.getElementById('addRecipe-form');
+const addIngredientForm = document.getElementById('addIngredient-form')
 
 async function newRecipeHandler(event) {
     event.preventDefault();
@@ -10,10 +11,7 @@ async function newRecipeHandler(event) {
     // Gets values from form fields
     const recipe_name = document.getElementById('add-recipe-name').value;
     const recipe_description = document.getElementById('add-recipe-desc').value;
-    const ingredient_name = document.getElementById('add-ingredient').value;
-    const quantity = document.getElementById('add-quantity').value;
-    const unit = document.getElementById('add-unit').value;
-    const image = document.getElementById('add-image').value;
+
     // Fetches using values then inserts data into body (to be extracted in the route (EXAMPLE: req.body.recipe_title) )
     const responseR = await fetch(`/api/recipes`, {
         method: 'POST',
@@ -33,6 +31,18 @@ async function newRecipeHandler(event) {
         // Log/Alert error
         alert(responseR.statusText);
     }
+
+}
+
+
+async function newIngredientHandler(event) {
+    event.preventDefault()
+
+    const ingredient_name = document.getElementById('add-ingredient').value;
+    const quantity = document.getElementById('add-quantity').value;
+    const unit = document.getElementById('add-unit').value;
+    // const image = document.getElementById('add-image').value; *LATER DEVELOPMENT*
+
     const responseI = await fetch(`/api/ingredients`, {
         method: 'POST',
         body: JSON.stringify({
@@ -50,27 +60,26 @@ async function newRecipeHandler(event) {
         // Log/Alert error
         alert(responseI.statusText);
     }
+
+    const responseRI = await fetch(`/api/recipeingredients`, {
+        method: 'POST',
+        body: JSON.stringify({
+            quantity,
+            unit
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    // IF response = OK refresh page. 
+    if (responseRI.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        // Log/Alert error
+        alert(responseRI.statusText);
+    }
 }
-
-const responseRI = await fetch(`/api/recipeingredients`, {
-    method: 'POST',
-    body: JSON.stringify({
-        quantity,
-        unit
-    }),
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
-
-// IF response = OK refresh page. 
-if (responseRI.ok) {
-    document.location.replace('/dashboard');
-} else {
-    // Log/Alert error
-    alert(responseRI.statusText);
-}
-
-
 // Add the event handler for the form submission
-addRecipeForm.addEventListener('submit', newRecipeHandler);
+addRecipeForm.addEventListener('submitRecipe', newRecipeHandler);
+addRecipeForm.addEventListener('submitIngredientbtn', newIngredientHandler);
