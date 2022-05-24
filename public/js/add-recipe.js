899a07af268b62a43b42122f8a56c1c62689377f
masontmorris <masonmorris8@gmail.com>
2022-05-23 21:51:09 -0400

    redirects to home page after adding a recipe (last time commiting directly to develop i promise)

--- public/js/add-recipe.js
+++ public/js/add-recipe.js
@@ -2,17 +2,17 @@
 //*****************NEEDS A WAY TO ADD INGREDIENTS of RECIPE to INGREDIENTS TABLE*****************//
 // ----- ----- ----- ----- EXECUTED IN DASHBOARD TEMPLATE --------------------------------//
 
-const addRecipeForm = document.getElementById('addRecipe-form');
-const addIngredientForm = document.getElementById('addIngredient-form')
-const ingredient_name = document.getElementById('add-ingredient');
-const quantity = document.getElementById('add-quantity')
-const unit = document.getElementById('add-unit')
-const recipe_name = document.getElementById('add-recipe-name')
-const recipe_description = document.getElementById('add-recipe-desc')
-const recipe_img = document.getElementById('add-recipe-img')
-const recipeList = document.getElementById('recipelist')
-const addIngredientbtn = document.getElementById('addIngredientbtn')
-const submitRecipebtn = document.getElementById('submitRecipebtn')
+const addRecipeForm = document.getElementById("addRecipe-form");
+const addIngredientForm = document.getElementById("addIngredient-form");
+const ingredient_name = document.getElementById("add-ingredient");
+const quantity = document.getElementById("add-quantity");
+const unit = document.getElementById("add-unit");
+const recipe_name = document.getElementById("add-recipe-name");
+const recipe_description = document.getElementById("add-recipe-desc");
+const recipe_img = document.getElementById("add-recipe-img");
+const recipeList = document.getElementById("recipelist");
+const addIngredientbtn = document.getElementById("addIngredientbtn");
+const submitRecipebtn = document.getElementById("submitRecipebtn");
 const ingredientData = [];
 
 async function newRecipeHandler(event) {
@@ -21,36 +21,30 @@ async function newRecipeHandler(event) {
         recipeName: recipe_name.value,
         recipeDesc: recipe_description.value,
         recipeimg: recipe_img.value,
-        ingredients: ingredientData
-
-    }
-    const newRecipe = await fetch('/addrecipes', {
-        method: 'POST',
+        ingredients: ingredientData,
+    };
+    const newRecipe = await fetch("/addrecipes", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(recipeDataFromUser)
-    })
-    console.log(":(")
+        body: JSON.stringify(recipeDataFromUser),
+    });
+    console.log(":(");
+    document.location.replace("/");
 }
 
-addIngredientbtn.addEventListener('click', function(event) {
-
+addIngredientbtn.addEventListener("click", function (event) {
     event.preventDefault();
     const ingredientName = ingredient_name.value;
     const quantityI = quantity.value;
-    const unitI = unit.value
-    recipeList.textContent += "\n" + ingredientName + "\n" + quantityI + "\n" + unitI + " || "
+    const unitI = unit.value;
+    recipeList.textContent += "\n" + ingredientName + "\n" + quantityI + "\n" + unitI + " || ";
 
     ingredientData.push({ ingredient_name: ingredientName, quantity: quantityI, unit: unitI });
-    ingredient_name.value = ''
-    quantity.value = ''
-    unit.value = ''
+    ingredient_name.value = "";
+    quantity.value = "";
+    unit.value = "";
+});
 
-
-})
-
-
-
-
-submitRecipebtn.addEventListener('click', newRecipeHandler);
+submitRecipebtn.addEventListener("click", newRecipeHandler);
