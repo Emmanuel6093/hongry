ac764409f2dee3621a2626886288571a123f67c6
masontmorris <masonmorris8@gmail.com>
2022-05-25 02:14:58 -0400

    images now display for recipes

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,18 @@
-// NEEDS ROUTES FOR ALL PAGES TO BE COMPLETED AND REQUIRED BELOW---** *Homepage/All-Recipes/Single-Recipe/Login*DONE*Register-User/Create-Recipe**UPLOAD IMAGE/Save Recipe/Search by Ingredients/Search by Recipe Name**   
+// NEEDS ROUTES FOR ALL PAGES TO BE COMPLETED AND REQUIRED BELOW---** *Homepage/All-Recipes/Single-Recipe/Login*DONE*Register-User/Create-Recipe**UPLOAD IMAGE/Save Recipe/Search by Ingredients/Search by Recipe Name**
 // FUTURE DEVELOPMENT IDEAS: "Comments/User Profiles/Video Embedding/RTMP Streaming service/Purchase Ingredients/Online Classes/Groups/Search by genre/Search by food category/"
+const express = require("express");
+const router = require("express").Router();
+const loginRoutes = require("./login-routes");
+const apiRoutes = require("./api/index.js");
+const homepageRoutes = require("./homepage-routes");
+const addrecipesroutes = require("./add-recipe-routes");
+const viewrecipesroutes = require("./view-recipes-routes");
 
-const router = require('express').Router();
-const loginRoutes = require('./login-routes');
-const apiRoutes = require('./api/index.js');
-const homepageRoutes = require('./homepage-routes')
-const addrecipesroutes = require('./add-recipe-routes')
-const viewrecipesroutes = require('./view-recipes-routes')
-
-router.use('/login', loginRoutes);
-router.use('/api', apiRoutes);
-router.use('/', homepageRoutes);
-router.use('/addrecipes', addrecipesroutes);
-router.use('/viewrecipes', viewrecipesroutes);
+router.use("/login", loginRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homepageRoutes);
+router.use("/addrecipes", addrecipesroutes);
+router.use("/viewrecipes", viewrecipesroutes);
+router.use("/images", express.static("public/recipe-images"));
 
 module.exports = router;
